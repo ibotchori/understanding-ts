f163db57fac1339c30163295a4811aec6f4470ef
Irakli <koregm@gmail.com>
2022-03-15 17:38:45 +0400

    Numbers, Strings and Booleans

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function add(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('Incorrect input!');
+function add(num1, num2, showResult, phrase) {
+    // if(typeof num1 !== 'number' || typeof num2 !== 'number' ) {
+    //   throw new Error('Incorrect input!')
+    // }
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
     }
-    return num1 + num2;
+    else {
+        return result;
+    }
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
-function add(num1: number, num2: number) {
-  if(typeof num1 !== 'number' || typeof num2 !== 'number' ) {
-    throw new Error('Incorrect input!')
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  // if(typeof num1 !== 'number' || typeof num2 !== 'number' ) {
+  //   throw new Error('Incorrect input!')
+  // }
+
+  let result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result)
+  } else {
+    return result
   }
-  return num1 + num2
 }
 
-const number1 = '5'
-const number2 = 2.8
 
 
-const result = add(number1, number2)
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: "
 
-console.log(result)
+add(number1, number2, printResult, resultPhrase);
+
