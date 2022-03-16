dd00b44c130cea364f50075cefa56cd02cbb5f8e
Irakli <koregm@gmail.com>
2022-03-16 23:38:34 +0400

    Union Types

--- app.js
+++ app.js
@@ -1,44 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   // Tuple type, array with 2 element, first number, second string
-//   role: [number, string];
-// } = {
-//   // const person = {
-//   name: "Irakli",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// };
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-// Enum type, enumerated list, labels just translated to incremented numbers, starting at 0, also you can set it manually
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 200] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    // const person = {
-    name: "Irakli",
-    age: 30,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.AUTHOR
-};
-if (person.role === Role.AUTHOR) {
-    console.log("Is author");
+// Union type
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else
+        [(result = input1.toString() + input2.toString())];
+    return result;
 }
-// person.role.push("admin"); // push is allow in tuple
-// person.role = [10, "admin", "user"]; // this is not allowed, you can add element only with push
-// person.role[1] = 10; // this is not allowed, second element must be number
-var favoriteActivities;
-favoriteActivities = ["Sports"];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!! property 'map' does not exist on type 'string'
-}
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine("Max", "Anna");
+console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,50 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   // Tuple type, array with 2 element, first number, second string
-//   role: [number, string];
-// } = {
-//   // const person = {
-//   name: "Irakli",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// };
+// Union type
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else [(result = input1.toString() + input2.toString())];
 
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-// Enum type, enumerated list, labels just translated to incremented numbers, starting at 0, also you can set it manually
-enum Role {
-  ADMIN = "ADMIN",
-  READ_ONLY = 100,
-  AUTHOR = 200,
+  return result;
 }
 
-const person = {
-  // const person = {
-  name: "Irakli",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.AUTHOR,
-};
+const combineAges = combine(30, 26);
+console.log(combineAges);
 
-if (person.role === Role.AUTHOR) {
-  console.log("Is author");
-}
-
-// person.role.push("admin"); // push is allow in tuple
-// person.role = [10, "admin", "user"]; // this is not allowed, you can add element only with push
-// person.role[1] = 10; // this is not allowed, second element must be number
-
-let favoriteActivities: any; // <-- any type
-favoriteActivities = ["Sports", 6];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!! property 'map' does not exist on type 'string'
-}
+const combineNames = combine("Max", "Anna");
+console.log(combineNames);
