f1c5b0cf6ada2d712d662f12999508b2e43e268d
Irakli <koregm@gmail.com>
2022-03-16 19:49:47 +0400

    Tuples

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
 var person = {
+    // const person = {
     name: "Irakli",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
+person.role.push("admin"); // push is allow in tuple
+person.role = [10, "admin", "user"]; // this is not allowed, you can add element only with push
+person.role[1] = 10; // this is not allowed, second element must be number
 var favoriteActivities;
 favoriteActivities = ["Sports"];
-console.log(person.name);
+console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!! property 'map' does not exist on type 'string'
 }
--- app.ts
+++ app.ts
@@ -1,17 +1,25 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  // Tuple type, array with 2 element, first number, second string
+  role: [number, string];
+} = {
+  // const person = {
   name: "Irakli",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+person.role.push("admin"); // push is allow in tuple
+person.role = [10, "admin", "user"]; // this is not allowed, you can add element only with push
+person.role[1] = 10; // this is not allowed, second element must be number
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
 
-console.log(person.name);
+console.log(person);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
