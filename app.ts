08d7a73136bb1bcea01bcf55ccf7fc5001cf61de
Irakli <koregm@gmail.com>
2022-03-17 18:37:17 +0400

    Function Return Types and Void

--- app.ts
+++ app.ts
@@ -1,34 +1,10 @@
-// Alias type
-type Combinbable = number | string;
-type ConversionDestructor = "as-number" | "as-text";
-
-function combine(
-  // Union type
-  input1: Combinbable,
-  input2: Combinbable,
-  resultConversion: ConversionDestructor // <--Literal types
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else [(result = input1.toString() + input2.toString())];
-
-  return result;
-  // if (resultConversion === "as-number") {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, "as-number");
-console.log(combineAges);
+// Void return type, means this function does not return anything
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combineStringAges = combine("30", "26", "as-number");
-console.log(combineStringAges);
-
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+printResult(add(2, 45));
