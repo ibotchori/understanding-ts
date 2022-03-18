d9584dbda53ca1f723775f7de91f84e77b92a2ef
Irakli <koregm@gmail.com>
2022-03-18 12:39:19 +0400

    Watch Mode

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-// The Unknown Type
-var userInput; // <-- this is better type than eny
-var userName;
-userInput = 5;
-userInput = "User";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-// The Never Type
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("An error occurred!", 500);
+var userName = "Irakli";
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,20 +1,3 @@
-// The Unknown Type
+const userName = "Irakli";
 
-let userInput: unknown; // <-- this is better type than eny
-let userName: string;
-
-userInput = 5;
-userInput = "User";
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-// The Never Type
-// set never to never return anything. for cases such this one, when app is crashed or infinite loop
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
-}
-
-generateError("An error occurred!", 500);
+console.log(userName);
