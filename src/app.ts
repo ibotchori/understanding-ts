16fb0beebd1cda7612fbc31f619673029326767e
Irakli <koregm@gmail.com>
2022-03-20 15:06:44 +0400

    Strict Compilation Options

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 const button = document.querySelector("button");
 
+function clickHandler(message: string) {
+  console.log("Clicked " + message);
+}
 // Comment
-button.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "you are welcome!"));
+}
